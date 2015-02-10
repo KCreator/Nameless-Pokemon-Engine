f23606e84b9226d09960782951eefabd04726866
thekittopiancreator <bryn@coolatai.com>
2015-02-11 10:56:56 +1100

    Fixes to DialogFrame

--- Pokemon Engine/DialogFrame.cpp
+++ Pokemon Engine/DialogFrame.cpp
@@ -27,14 +27,14 @@ void DialogFrame::Render( int StartX, int StartY, int EndX, int EndY )
 	//Top
 	SDL_RenderCopy( gRenderer, m_texture, &GetRect(0, 0, 8, 8 ), &GetRect( StartX, StartY, 8, 8 ) );
 	SDL_RenderCopy( gRenderer, m_texture, &GetRect(8, 0, 8, 8 ), &GetRect( StartX + 8, StartY, EndX-8, 8 ) );
-	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 0, 8, 8 ), &GetRect( EndX - 8, StartY, 8, 8 ) );
+	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 0, 8, 8 ), &GetRect( StartX + EndX - 8, StartY, 8, 8 ) );
 
 	SDL_RenderCopy( gRenderer, m_texture, &GetRect(0, 8, 8, 8 ), &GetRect( StartX + 0 - 0, StartY+8, 8, EndY - 8 ) );
 	SDL_RenderCopy( gRenderer, m_texture, &GetRect(8, 8, 8, 8 ), &GetRect( StartX + 8 - 0, StartY+8, EndX-8, EndY - 8 ) );
-	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 8, 8, 8 ), &GetRect( EndX - 8, StartY+8, 8, EndY - 8 ) );
+	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 8, 8, 8 ), &GetRect( StartX + EndX - 8, StartY+8, 8, EndY - 8 ) );
 
 	//Bottom
-	SDL_RenderCopy( gRenderer, m_texture, &GetRect(0, 16, 8, 8 ), &GetRect( StartX, EndY-8, 8, 8 ) );
-	SDL_RenderCopy( gRenderer, m_texture, &GetRect(8, 16, 8, 8 ), &GetRect( StartX + 8, EndY-8, EndX-8, 8 ) );
-	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 16, 8, 8 ), &GetRect( EndX - 8, EndY-8, 8, 8 ) );
+	SDL_RenderCopy( gRenderer, m_texture, &GetRect(0, 16, 8, 8 ), &GetRect( StartX, StartY+EndY-8, 8, 8 ) );
+	SDL_RenderCopy( gRenderer, m_texture, &GetRect(8, 16, 8, 8 ), &GetRect( StartX + 8, StartY+EndY-8, EndX-8, 8 ) );
+	SDL_RenderCopy( gRenderer, m_texture, &GetRect(16, 16, 8, 8 ), &GetRect( StartX + EndX - 8, StartY+EndY-8, 8, 8 ) );
 }
